df59667043b63f2f4643736fe730f6117726e318
avivlubell <aviv.lubell@pathovagtm.com>
2026-03-25 13:41:26 -0400

    Fix file input: use offscreen positioning instead of zero dimensions

--- app/page.tsx
+++ app/page.tsx
@@ -307,7 +307,7 @@ export default function HomePage() {
               ref={fileInputRef}
               type="file"
               multiple
-              className="absolute w-0 h-0 opacity-0 overflow-hidden"
+              style={{ position: 'absolute', left: '-9999px', opacity: 0 }}
               onChange={(e) => {
                 if (e.target.files) setFiles((prev) => [...prev, ...Array.from(e.target.files!)]);
                 e.target.value = '';
